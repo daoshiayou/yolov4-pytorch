e5cd5323b7fceacbd2a3bf27d7f29748513239ed
ChenJ <535699028@qq.com>
2021-03-20 14:10:11 +0800

    modified for my dataset

--- train.py
+++ train.py
@@ -29,35 +29,41 @@ def get_classes(classes_path):
     class_names = [c.strip() for c in class_names]
     return class_names
 
+
 def get_anchors(anchors_path):
     '''loads the anchors from a file'''
     with open(anchors_path) as f:
         anchors = f.readline()
     anchors = [float(x) for x in anchors.split(',')]
-    return np.array(anchors).reshape([-1,3,2])[::-1,:,:]
+    return np.array(anchors).reshape([-1, 3, 2])[::-1, :, :]
+
 
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
 
-        
-def fit_one_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,genval,Epoch,cuda):
+
+def fit_one_epoch(net, yolo_losses, epoch, epoch_size, epoch_size_val, gen, genval, Epoch, cuda):
     total_loss = 0
     val_loss = 0
 
     net.train()
-    with tqdm(total=epoch_size,desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
+    with tqdm(total=epoch_size, desc=f'Epoch {epoch + 1}/{Epoch}', postfix=dict, mininterval=0.3) as pbar:
         for iteration, batch in enumerate(gen):
             if iteration >= epoch_size:
                 break
             images, targets = batch[0], batch[1]
             with torch.no_grad():
                 if cuda:
-                    images = Variable(torch.from_numpy(images).type(torch.FloatTensor)).cuda()
-                    targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)) for ann in targets]
+                    images = Variable(torch.from_numpy(
+                        images).type(torch.FloatTensor)).cuda()
+                    targets = [Variable(torch.from_numpy(ann).type(
+                        torch.FloatTensor)) for ann in targets]
                 else:
-                    images = Variable(torch.from_numpy(images).type(torch.FloatTensor))
-                    targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)) for ann in targets]
+                    images = Variable(torch.from_numpy(
+                        images).type(torch.FloatTensor))
+                    targets = [Variable(torch.from_numpy(ann).type(
+                        torch.FloatTensor)) for ann in targets]
 
             #----------------------#
             #   清零梯度
@@ -85,14 +91,14 @@ def fit_one_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,genval,Epo
             optimizer.step()
 
             total_loss += loss.item()
-            
-            pbar.set_postfix(**{'total_loss': total_loss / (iteration + 1), 
-                                'lr'        : get_lr(optimizer)})
+
+            pbar.set_postfix(**{'total_loss': total_loss / (iteration + 1),
+                                'lr': get_lr(optimizer)})
             pbar.update(1)
 
     net.eval()
     print('Start Validation')
-    with tqdm(total=epoch_size_val, desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
+    with tqdm(total=epoch_size_val, desc=f'Epoch {epoch + 1}/{Epoch}', postfix=dict, mininterval=0.3) as pbar:
         for iteration, batch in enumerate(genval):
             if iteration >= epoch_size_val:
                 break
@@ -100,17 +106,22 @@ def fit_one_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,genval,Epo
 
             with torch.no_grad():
                 if cuda:
-                    images_val = Variable(torch.from_numpy(images_val).type(torch.FloatTensor)).cuda()
-                    targets_val = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)) for ann in targets_val]
+                    images_val = Variable(torch.from_numpy(
+                        images_val).type(torch.FloatTensor)).cuda()
+                    targets_val = [Variable(torch.from_numpy(ann).type(
+                        torch.FloatTensor)) for ann in targets_val]
                 else:
-                    images_val = Variable(torch.from_numpy(images_val).type(torch.FloatTensor))
-                    targets_val = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)) for ann in targets_val]
+                    images_val = Variable(torch.from_numpy(
+                        images_val).type(torch.FloatTensor))
+                    targets_val = [Variable(torch.from_numpy(ann).type(
+                        torch.FloatTensor)) for ann in targets_val]
                 optimizer.zero_grad()
                 outputs = net(images_val)
                 losses = []
                 num_pos_all = 0
                 for i in range(3):
-                    loss_item, num_pos = yolo_losses[i](outputs[i], targets_val)
+                    loss_item, num_pos = yolo_losses[i](
+                        outputs[i], targets_val)
                     losses.append(loss_item)
                     num_pos_all += num_pos
                 loss = sum(losses) / num_pos_all
@@ -118,11 +129,14 @@ def fit_one_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,genval,Epo
             pbar.set_postfix(**{'total_loss': val_loss / (iteration + 1)})
             pbar.update(1)
     print('Finish Validation')
-    print('Epoch:'+ str(epoch+1) + '/' + str(Epoch))
-    print('Total Loss: %.4f || Val Loss: %.4f ' % (total_loss/(epoch_size+1),val_loss/(epoch_size_val+1)))
+    print('Epoch:' + str(epoch+1) + '/' + str(Epoch))
+    print('Total Loss: %.4f || Val Loss: %.4f ' %
+          (total_loss/(epoch_size+1), val_loss/(epoch_size_val+1)))
 
     print('Saving state, iter:', str(epoch+1))
-    torch.save(model.state_dict(), 'logs/Epoch%d-Total_Loss%.4f-Val_Loss%.4f.pth'%((epoch+1),total_loss/(epoch_size+1),val_loss/(epoch_size_val+1)))
+    torch.save(model.state_dict(), 'logs/Epoch%d-Total_Loss%.4f-Val_Loss%.4f.pth' %
+               ((epoch+1), total_loss/(epoch_size+1), val_loss/(epoch_size_val+1)))
+
 
 #----------------------------------------------------#
 #   检测精度mAP和pr曲线计算参考视频
@@ -148,24 +162,24 @@ if __name__ == "__main__":
     #   显存比较小可以使用416x416
     #   显存比较大可以使用608x608
     #-------------------------------#
-    input_shape = (416,416)
+    input_shape = (416, 416)
 
     #----------------------------------------------------#
     #   classes和anchor的路径，非常重要
     #   训练前一定要修改classes_path，使其对应自己的数据集
     #----------------------------------------------------#
     anchors_path = 'model_data/yolo_anchors.txt'
-    classes_path = 'model_data/voc_classes.txt'   
+    classes_path = 'model_data/ai_classes.txt'
     #----------------------------------------------------#
     #   获取classes和anchor
     #----------------------------------------------------#
     class_names = get_classes(classes_path)
     anchors = get_anchors(anchors_path)
     num_classes = len(class_names)
-    
+
     #------------------------------------------------------#
     #   Yolov4的tricks应用
-    #   mosaic 马赛克数据增强 True or False 
+    #   mosaic 马赛克数据增强 True or False
     #   实际测试时mosaic数据增强并不稳定，所以默认为False
     #   Cosine_scheduler 余弦退火学习率 True or False
     #   label_smoothing 标签平滑 0.01以下一般 如0.01、0.005
@@ -188,7 +202,8 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model_dict = model.state_dict()
     pretrained_dict = torch.load(model_path, map_location=device)
-    pretrained_dict = {k: v for k, v in pretrained_dict.items() if np.shape(model_dict[k]) ==  np.shape(v)}
+    pretrained_dict = {k: v for k, v in pretrained_dict.items(
+    ) if np.shape(model_dict[k]) == np.shape(v)}
     model_dict.update(pretrained_dict)
     model.load_state_dict(model_dict)
     print('Finished!')
@@ -203,8 +218,8 @@ if __name__ == "__main__":
     # 建立loss函数
     yolo_losses = []
     for i in range(3):
-        yolo_losses.append(YOLOLoss(np.reshape(anchors,[-1,2]),num_classes, \
-                                (input_shape[1], input_shape[0]), smoooth_label, Cuda, normalize))
+        yolo_losses.append(YOLOLoss(np.reshape(anchors, [-1, 2]), num_classes,
+                                    (input_shape[1], input_shape[0]), smoooth_label, Cuda, normalize))
 
     #----------------------------------------------------#
     #   获得图片路径和标签
@@ -223,7 +238,7 @@ if __name__ == "__main__":
     np.random.seed(None)
     num_val = int(len(lines)*val_split)
     num_train = len(lines) - num_val
-    
+
     #------------------------------------------------------#
     #   主干特征提取网络特征通用，冻结训练可以加快训练速度
     #   也可以在训练初期防止权值被破坏。
@@ -237,29 +252,33 @@ if __name__ == "__main__":
         Batch_size = 4
         Init_Epoch = 0
         Freeze_Epoch = 50
-        
+
         #----------------------------------------------------------------------------#
         #   我在实际测试时，发现optimizer的weight_decay起到了反作用，
         #   所以去除掉了weight_decay，大家也可以开起来试试，一般是weight_decay=5e-4
         #----------------------------------------------------------------------------#
-        optimizer = optim.Adam(net.parameters(),lr)
+        optimizer = optim.Adam(net.parameters(), lr)
         if Cosine_lr:
-            lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=5, eta_min=1e-5)
+            lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(
+                optimizer, T_max=5, eta_min=1e-5)
         else:
-            lr_scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=1,gamma=0.92)
+            lr_scheduler = optim.lr_scheduler.StepLR(
+                optimizer, step_size=1, gamma=0.92)
 
         if Use_Data_Loader:
-            train_dataset = YoloDataset(lines[:num_train], (input_shape[0], input_shape[1]), mosaic=mosaic, is_train=True)
-            val_dataset = YoloDataset(lines[num_train:], (input_shape[0], input_shape[1]), mosaic=False, is_train=False)
+            train_dataset = YoloDataset(
+                lines[:num_train], (input_shape[0], input_shape[1]), mosaic=mosaic, is_train=True)
+            val_dataset = YoloDataset(
+                lines[num_train:], (input_shape[0], input_shape[1]), mosaic=False, is_train=False)
             gen = DataLoader(train_dataset, shuffle=True, batch_size=Batch_size, num_workers=4, pin_memory=True,
-                                    drop_last=True, collate_fn=yolo_dataset_collate)
-            gen_val = DataLoader(val_dataset, shuffle=True, batch_size=Batch_size, num_workers=4,pin_memory=True, 
-                                    drop_last=True, collate_fn=yolo_dataset_collate)
+                             drop_last=True, collate_fn=yolo_dataset_collate)
+            gen_val = DataLoader(val_dataset, shuffle=True, batch_size=Batch_size, num_workers=4, pin_memory=True,
+                                 drop_last=True, collate_fn=yolo_dataset_collate)
         else:
             gen = Generator(Batch_size, lines[:num_train],
-                            (input_shape[0], input_shape[1])).generate(train=True, mosaic = mosaic)
+                            (input_shape[0], input_shape[1])).generate(train=True, mosaic=mosaic)
             gen_val = Generator(Batch_size, lines[num_train:],
-                            (input_shape[0], input_shape[1])).generate(train=False, mosaic = mosaic)
+                                (input_shape[0], input_shape[1])).generate(train=False, mosaic=mosaic)
 
         epoch_size = max(1, num_train//Batch_size)
         epoch_size_val = num_val//Batch_size
@@ -269,8 +288,9 @@ if __name__ == "__main__":
         for param in model.backbone.parameters():
             param.requires_grad = False
 
-        for epoch in range(Init_Epoch,Freeze_Epoch):
-            fit_one_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,gen_val,Freeze_Epoch,Cuda)
+        for epoch in range(Init_Epoch, Freeze_Epoch):
+            fit_one_epoch(net, yolo_losses, epoch, epoch_size,
+                          epoch_size_val, gen, gen_val, Freeze_Epoch, Cuda)
             lr_scheduler.step()
 
     if True:
@@ -283,24 +303,28 @@ if __name__ == "__main__":
         #   我在实际测试时，发现optimizer的weight_decay起到了反作用，
         #   所以去除掉了weight_decay，大家也可以开起来试试，一般是weight_decay=5e-4
         #----------------------------------------------------------------------------#
-        optimizer = optim.Adam(net.parameters(),lr)
+        optimizer = optim.Adam(net.parameters(), lr)
         if Cosine_lr:
-            lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=5, eta_min=1e-5)
+            lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(
+                optimizer, T_max=5, eta_min=1e-5)
         else:
-            lr_scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=1,gamma=0.92)
+            lr_scheduler = optim.lr_scheduler.StepLR(
+                optimizer, step_size=1, gamma=0.92)
 
         if Use_Data_Loader:
-            train_dataset = YoloDataset(lines[:num_train], (input_shape[0], input_shape[1]), mosaic=mosaic, is_train=True)
-            val_dataset = YoloDataset(lines[num_train:], (input_shape[0], input_shape[1]), mosaic=False, is_train=False)
+            train_dataset = YoloDataset(
+                lines[:num_train], (input_shape[0], input_shape[1]), mosaic=mosaic, is_train=True)
+            val_dataset = YoloDataset(
+                lines[num_train:], (input_shape[0], input_shape[1]), mosaic=False, is_train=False)
             gen = DataLoader(train_dataset, shuffle=True, batch_size=Batch_size, num_workers=4, pin_memory=True,
-                                    drop_last=True, collate_fn=yolo_dataset_collate)
-            gen_val = DataLoader(val_dataset, shuffle=True, batch_size=Batch_size, num_workers=4,pin_memory=True, 
-                                    drop_last=True, collate_fn=yolo_dataset_collate)
+                             drop_last=True, collate_fn=yolo_dataset_collate)
+            gen_val = DataLoader(val_dataset, shuffle=True, batch_size=Batch_size, num_workers=4, pin_memory=True,
+                                 drop_last=True, collate_fn=yolo_dataset_collate)
         else:
             gen = Generator(Batch_size, lines[:num_train],
-                            (input_shape[0], input_shape[1])).generate(train=True, mosaic = mosaic)
+                            (input_shape[0], input_shape[1])).generate(train=True, mosaic=mosaic)
             gen_val = Generator(Batch_size, lines[num_train:],
-                            (input_shape[0], input_shape[1])).generate(train=False, mosaic = mosaic)
+                                (input_shape[0], input_shape[1])).generate(train=False, mosaic=mosaic)
 
         epoch_size = max(1, num_train//Batch_size)
         epoch_size_val = num_val//Batch_size
@@ -310,6 +334,7 @@ if __name__ == "__main__":
         for param in model.backbone.parameters():
             param.requires_grad = True
 
-        for epoch in range(Freeze_Epoch,Unfreeze_Epoch):
-            fit_one_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,gen_val,Unfreeze_Epoch,Cuda)
+        for epoch in range(Freeze_Epoch, Unfreeze_Epoch):
+            fit_one_epoch(net, yolo_losses, epoch, epoch_size,
+                          epoch_size_val, gen, gen_val, Unfreeze_Epoch, Cuda)
             lr_scheduler.step()
